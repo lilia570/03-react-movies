42fbbf2f8c29dce0ead417fade1700c9c9d53030
lilia570 <liliapaslavska570@gmail.com>
2025-12-10 02:39:57 +0200

    correct

--- src/components/MovieModal/MovieModal.tsx
+++ src/components/MovieModal/MovieModal.tsx
@@ -30,17 +30,19 @@ export default function MovieModal({ movie, onClose }: MovieModalProps) {
   return createPortal(
     <div className={styles.backdrop} onClick={handleBackdropClick}>
       <div className={styles.modal} onClick={(e) => e.stopPropagation()}>
+        <div className={styles.content}> 
         <h2>{movie.title}</h2>
         <p>{movie.overview}</p>
         <p><strong>Release date:</strong> {movie.release_date}</p>
-        <p><strong>Rating:</strong> {movie.vote_average}</p>
+          <p><strong>Rating:</strong> {movie.vote_average}</p>
+        </div>
         {movie.backdrop_path && (
-          <img
+          <img className="styles.image"
             src={`https://image.tmdb.org/t/p/w400${movie.backdrop_path}`}
             alt="Poster"
           />
         )}
-        <button className={styles.close} onClick={onClose}>
+        <button className={styles.closeButton} onClick={onClose}>
           Close
         </button>
       </div>
