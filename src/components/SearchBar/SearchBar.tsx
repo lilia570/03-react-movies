fb8bab422298080257cae57b80c267a7a48beefb
lilia570 <liliapaslavska570@gmail.com>
2025-12-15 21:31:18 +0200

    correct SearchBar

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,4 +1,3 @@
-import type {FormEvent} from "react";
 import styles from "./SearchBar.module.css";
 import { toast } from "react-hot-toast";
 interface SearchBarProps {
@@ -6,10 +5,8 @@ interface SearchBarProps {
 }
 
 export default function SearchBar({ onSubmit }: SearchBarProps) {
-    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-      e.preventDefault();
-        const formData = new FormData(e.currentTarget);
-    const query = formData.get("query")?.toString().trim();
+    const handleSubmit = (formData: FormData) => {
+    const query = (formData.get("query") as string)?.trim()?? '';
 
     if (!query) {
       toast.error("Please enter a search term");
@@ -17,7 +14,7 @@ export default function SearchBar({ onSubmit }: SearchBarProps) {
     }
 
     onSubmit(query);
-    e.currentTarget.reset();
+     
 
   };
 
@@ -32,7 +29,7 @@ export default function SearchBar({ onSubmit }: SearchBarProps) {
         >
           Powered by TMDB
         </a>
-    <form className={styles.form} onSubmit={handleSubmit}>
+    <form className={styles.form} action={handleSubmit}>
       <input
         className={styles.input}
         type="text"
